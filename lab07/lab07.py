afd2a830bcb7dd6e39f029d2d8354d7fb7413839
Hirutsuki <61302403+Hirutsuki@users.noreply.github.com>
2022-07-28 18:17:58 +0800

    lab07

---
+++ lab07/lab07.py
@@ -0,0 +1,216 @@
+""" Lab 07: Generators, Linked Lists, and Trees """
+
+
+# Linked Lists
+
+from ntpath import altsep
+from operator import truediv
+
+
+def link_to_list(link):
+    """Takes a linked list and returns a Python list with the same elements.
+
+    >>> link = Link(1, Link(2, Link(3, Link(4))))
+    >>> link_to_list(link)
+    [1, 2, 3, 4]
+    >>> link_to_list(Link.empty)
+    []
+    """
+    "*** YOUR CODE HERE ***"
+    # if link is Link.empty:
+    #     return []
+    # if not isinstance(link, Link):
+    #     return link
+    # return [link_to_list(link.first)] + link_to_list(link.rest)
+    link_list = []
+    while link is not Link.empty:
+        if not isinstance(link.first, Link):
+            link_list.append(link.first)
+        else:
+            link_list.append(link_to_list(link.first))
+        link = link.rest
+    return link_list
+
+# Trees
+
+
+def cumulative_mul(t):
+    """Mutates t so that each node's label becomes the product of all labels in
+    the corresponding subtree rooted at t.
+
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> cumulative_mul(t)
+    >>> t
+    Tree(105, [Tree(15, [Tree(5)]), Tree(7)])
+    """
+    "*** YOUR CODE HERE ***"
+    def f(t):
+        if t.is_leaf():
+            return t.label
+
+        sub_labels = [f(b) for b in t.branches]
+        sub_product = 1
+        for label in sub_labels:
+            sub_product *= label
+
+        t.label = t.label*sub_product
+        return t.label
+
+    f(t)
+
+
+# Link List Class
+
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+# Tree ADT
+
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
+
+def has_cycle(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle(t)
+    False
+    >>> u = Link(2, Link(2, Link(2)))
+    >>> has_cycle(u)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    # link_set = set()
+    # while link is not Link.empty:
+    #     if link in link_set:
+    #         return True
+    #     link_set.add(link)
+    #     link = link.rest
+    # return False
+    l = link
+    while l.rest is not Link.empty:
+        if l.rest is link:
+            return True
+        l = l.rest
+    return False
+
+
+def has_cycle_constant(link):
+    """Return whether link contains a cycle.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.rest.rest.rest = s
+    >>> has_cycle_constant(s)
+    True
+    >>> t = Link(1, Link(2, Link(3)))
+    >>> has_cycle_constant(t)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    fast, slow = link, link
+    while fast.rest is not Link.empty:
+        fast = fast.rest
+        if fast.rest is Link.empty:
+            return False
+        fast, slow = fast.rest, slow.rest
+        if fast is slow:
+            return True
+    return False
+
+
+def reverse_other(t):
+    """Mutates the tree such that nodes on every other (odd-depth) level
+    have the labels of their branches all reversed.
+
+    >>> t = Tree(1, [Tree(2), Tree(3), Tree(4)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(4), Tree(3), Tree(2)])
+    >>> t = Tree(1, [Tree(2, [Tree(3, [Tree(4), Tree(5)]), Tree(6, [Tree(7)])]), Tree(8)])
+    >>> reverse_other(t)
+    >>> t
+    Tree(1, [Tree(8, [Tree(3, [Tree(5), Tree(4)]), Tree(6, [Tree(7)])]), Tree(2)])
+    """
+    "*** YOUR CODE HERE ***"
+    for i in range(len(t.branches)//2):
+        t.branches[i].label, t.branches[-(i+1)].label = t.branches[-(i+1)
+                                                                   ].label, t.branches[i].label
+    for odd_branches in [b.branches for b in t.branches]:
+        for b in odd_branches:
+            reverse_other(b)
