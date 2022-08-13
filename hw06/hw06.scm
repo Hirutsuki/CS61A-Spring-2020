d836682e5058d7699e4a6a369591e2ba10de6618
Hirutsuki <61302403+Hirutsuki@users.noreply.github.com>
2022-08-13 23:10:25 +0800

    reformatted with editor

--- hw06/hw06.scm
+++ hw06/hw06.scm
@@ -1,41 +1,41 @@
-;;;;;;;;;;;;;;;
-;; Questions ;;
-;;;;;;;;;;;;;;;
-
+; ;;;;;;;;;;;;;;
+; ; Questions ;;
+; ;;;;;;;;;;;;;;
 ; Scheme
+(define (cddr s) (cdr (cdr s)))
 
-(define (cddr s)
-  (cdr (cdr s)))
+(define (cadr s) (car (cdr s)))
 
-(define (cadr s)
-  (car (cdr s))
-)
-
-(define (caddr s)
-  (car (cddr s))
-)
+(define (caddr s) (car (cddr s)))
 
 (define (sign x)
-  (cond ((> x 0) 1)
-        ((< x 0) -1)
-        (else 0))
+  (cond 
+    ((> x 0) 1)
+    ((< x 0) -1)
+    (else    0)
+  )
 )
 
 (define (square x) (* x x))
 
 (define (pow b n)
-  (cond ((zero? n) 1)
-        ((= n 1) b)
-        ((even? n) (square (pow b (/ n 2))))
-        (else (* b (pow b (- n 1)))))
+  (cond 
+    ((zero? n) 1)
+    ((= n 1)   b)
+    ((even? n) (square (pow b (/ n 2))))
+    (else      (* b (pow b (- n 1))))
+  )
 )
 
 (define (unique s)
-  (if (null? s) nil
+  (if (null? s)
+      nil
       (cons (car s)
-        (unique
-          (filter (lambda (x) (not (equal? x (car s)))) (cdr s))
-        )
+            (unique
+             (filter (lambda (x) (not (equal? x (car s))))
+                     (cdr s)
+             )
+            )
       )
   )
 )
